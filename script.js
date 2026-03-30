7d93defa952bf25af39a937518043e680c14e5a1
SurajYT69 <118313343+SurajYT69@users.noreply.github.com>
2026-03-30 18:20:40 +0530

    script.js

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ let orders = 48;
 
 document.getElementById("sales").innerText = "₹" + sales;
 document.getElementById("orders").innerText = orders;
+
+// Chart Data
+const ctx = document.getElementById('salesChart').getContext('2d');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: [{
+            label: 'Weekly Sales (₹)',
+            data: [5000, 7000, 8000, 6000, 9000, 12000, 15000],
+            borderWidth: 2
+        }]
+    }
+});
